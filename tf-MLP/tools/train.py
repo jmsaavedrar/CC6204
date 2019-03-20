47ff2e964f5a53c7e6f8bb8470db047b363cc6e2
jose.saavedra <jose.saavedra@orand.cl>
2019-03-20 12:57:55 -0300

    tf-mlp

--- tf-MLP/tools/train.py
+++ tf-MLP/tools/train.py
@@ -9,7 +9,7 @@ if __name__ == "__main__" :
               "data_dir" : "/home/vision/smb-datasets/SBIR/QuickDraw-Animals",              
               "learning_rate" : 0.001,  #for gd method a smaller lr is required example 0.01
               "number_of_classes" : 12,
-              "number_of_iterations" : 10000,
+              "number_of_iterations" : 40000,
               "batch_size" : 80,
               "data_size" : 12000,
         }
